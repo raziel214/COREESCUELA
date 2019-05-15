3a9ba1b295362b5db27bb68014ec99386f58c73d
raziel214 <raziel214@users.noreply.github.com>
2019-05-14 21:25:24 -0500

    start new course C# Net.Core

--- App/EscuelaEngine.cs
+++ App/EscuelaEngine.cs
@@ -40,7 +40,7 @@ namespace CoreEscuela
 
                         for(int i=0; i<5;i++){
 
-                            var ev=new Evaluaciones{
+                            var ev=new Evaluacion{
                                 Asignatura=asignatura,
                                 Nombre=$"{asignatura.Nombre} Ev#{i+1}",
                                 Nota = (float)(5*rnd.NextDouble()),
--- Entidades/Alumno.cs
+++ Entidades/Alumno.cs
@@ -4,17 +4,14 @@ using System.Collections.Generic;
 namespace CoreEscuela.Entidades
 {
     public class Alumno
-    {public Alumno()
-        {
-            UniqueId = Guid.NewGuid().ToString();
-            Evaluaciones = new List<Evaluaciones>();
-        }
+    {
         public string UniqueId { get; private set; }
         public string Nombre { get; set; }
 
         //public Alumno() => UniqueId = Guid.NewGuid().ToString();
         //Evaluaciones = new List<Evaluaciones>();
-        public List<Evaluaciones> Evaluaciones { get; set; }
+        public List<Evaluacion> Evaluaciones { get; set; } = new List<Evaluacion>();
+        public Alumno()=>UniqueId=Guid.NewGuid().ToString();
 
         
     }
